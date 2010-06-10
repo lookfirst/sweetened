49f6f41bb1eb0f21de99bff5a340650bdd9c97da
latchkey <latchkey@8b337d6a-a016-b5bd-8cc1-d974dc75d957>
2010-06-10 23:31:13 +0000

    things are working now.

--- trunk/src/com/googlecode/sweetened/typedef/SweetenedScope.java
+++ trunk/src/com/googlecode/sweetened/typedef/SweetenedScope.java
@@ -2,20 +2,23 @@ package com.googlecode.sweetened.typedef;
 
 /**
  * Defines the scope of a jar file. Right now, there are
- * only three options: runtime, compile and all.
+ * only four options: runtime, compile, unit and all.
  */
 public enum SweetenedScope {
     RUNTIME("runtime"), COMPILE("compile"), UNIT("unit"), ALL("all");
     private String scope;
 
+    /** */
     private SweetenedScope(String scope) {
         this.scope = scope;
     }
 
+    /** */
     public String getScope() {
         return scope;
     }
 
+    /** */
     public static SweetenedScope safeValueOf(String scope) {
         try {
             return valueOf(scope.toUpperCase());
