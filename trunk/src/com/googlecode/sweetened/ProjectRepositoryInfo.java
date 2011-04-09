853ce110b04cdd992ad5f641cb63be741d12b9b1
jnamnath <jnamnath@8b337d6a-a016-b5bd-8cc1-d974dc75d957>
2011-04-09 01:25:13 +0000

    A couple more comments. JN

--- trunk/src/com/googlecode/sweetened/ProjectRepositoryInfo.java
+++ trunk/src/com/googlecode/sweetened/ProjectRepositoryInfo.java
@@ -2,8 +2,16 @@ package com.googlecode.sweetened;
 
 import java.io.File;
 
+/**
+ * @author jnamnath
+ *
+ */
 public interface ProjectRepositoryInfo {
 	
+	/**
+	 * 
+	 * @return true if the repository info is available.
+	 */
 	public boolean init(File baseDir);
 	
 	public String getBranch();
