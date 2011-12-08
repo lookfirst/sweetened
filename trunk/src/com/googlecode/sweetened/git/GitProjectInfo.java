e50320db62cdb91f125711093b6ecd87ae57e6a9
latchkey <latchkey@8b337d6a-a016-b5bd-8cc1-d974dc75d957>
2011-12-08 17:06:57 +0000

    upgrade to latest jgit version.

--- trunk/src/com/googlecode/sweetened/git/GitProjectInfo.java
+++ trunk/src/com/googlecode/sweetened/git/GitProjectInfo.java
@@ -10,36 +10,33 @@ import org.eclipse.jgit.storage.file.FileRepository;
 import com.googlecode.sweetened.ProjectRepositoryInfo;
 
 /**
- * 
  * Uses jgit to determine the head revision of the current
  * working branch.
  *
- *
  * The output would be something like:
  *  master-path/to/basedir or master-myrepo
- *  
- * @author jnamnath
  *
+ * @author jnamnath
  */
 public class GitProjectInfo implements ProjectRepositoryInfo {
 
-    private static final String GIT_DIR_NAME = ".git";
+	private static final String GIT_DIR_NAME = ".git";
 	private static final String HEAD_REVISION = "HEAD";
-    
+
 	private String branch = null;
-    private String revision = null;
-	
+	private String revision = null;
+
 	@Override
 	public boolean init(File baseDir) {
 		try {
-			
+
 			// find the root repository directory
 			File rootFolder = getRootBranchFolder(baseDir);
 			if (rootFolder == null) {
 				// found no git dir
 				return false;
 			}
-			
+
 			// now build the last part of the branch name from the path relative to the root dir
 			File curr = baseDir;
 			String branch = curr.getName();
@@ -47,10 +44,10 @@ public class GitProjectInfo implements ProjectRepositoryInfo {
 				curr = new File(curr.getParent());
 				branch = curr.getName() + File.separator + branch;
 			}
-			
+
 			Repository repository = new FileRepository(getGitDir(rootFolder));
 			this.branch = repository.getBranch() + "-" + branch;
-			
+
 			ObjectId head = repository.resolve(HEAD_REVISION);
 			this.revision = head.getName();
 		} catch (Exception e) {
@@ -64,7 +61,7 @@ public class GitProjectInfo implements ProjectRepositoryInfo {
 		if (curr == null) {
 			return null;
 		}
-		
+
 		File gitDir = getGitDir(curr);
 		while (!gitDir.exists() || !gitDir.isDirectory()) {
 			if (curr.getParent() == null) {
@@ -76,11 +73,11 @@ public class GitProjectInfo implements ProjectRepositoryInfo {
 		// found a .git dir somewhere up the tree, so return curr dir
 		return curr;
 	}
-	
+
 	private File getGitDir(File dir) {
 		return new File(dir.getAbsolutePath() + File.separator + GIT_DIR_NAME);
 	}
-	
+
 	@Override
 	public String getBranch() {
 		return this.branch;
@@ -90,5 +87,5 @@ public class GitProjectInfo implements ProjectRepositoryInfo {
 	public String getRevision() {
 		return this.revision;
 	}
-	
+
 }
